1f469b3592b78e44e619d01bb6ae08821b4a9b30
AIiiMi <vebcxejfhx22910@example.com>
2026-01-20 16:51:56 +0900

    Fix syntax error in config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
-schema: ({ image }) => z.object({
-	title: z.string(),
-	description: z.string(),
-	pubDate: z.coerce.date(),
-	updatedDate: z.coerce.date().optional(),
-	version: z.string(),
-	category: z.enum(['News', 'Mods', 'Column', 'Tech']),
-	tags: z.array(z.string()).default([]),
-	heroImage: image().optional(),
-}),
+const blog = defineCollection({
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		version: z.string(),
+		category: z.enum(['News', 'Mods', 'Column', 'Tech']),
+		tags: z.array(z.string()).default([]),
+		heroImage: image().optional(),
+	}),
 });
 
 export const collections = { blog };
